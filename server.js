71fa989496148e0d3b06e4dd4e9e06266899364e
John Stuart <31050027+johnstuart670@users.noreply.github.com>
2018-06-02 11:48:17 -0600

    initialized with all the files for the basic structure

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Require all database models
+var db = require("./models");
+
+// Set port to listen on
+var PORT = process.env.PORT || 8080;
+// Initialize Express
+var app = express();
+
+// Express-handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(logger("dev"));
+
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// static directory to have the ability to style the pages and host images.
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+
+// Require our routes
+require('./routes/routes.js')(app);
+
+// Start the server
+app.listen(PORT, function () {
+  console.log("App running on port " + PORT);
+});
